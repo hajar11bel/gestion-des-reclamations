558c34832e15877f375b4ef446b50b3d5ede967b
younes <lahlaliayounes@gmail.com>
2024-02-29 14:22:48 +0100

    tp

--- routes/personnel.php
+++ routes/personnel.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::prefix('personnel')->group(function () {
+
 
 
 Route::view('/personnel/login','personnel.login');
@@ -24,20 +24,23 @@ Route::post('/personnel/reg',[PersonnelController::class,'reg']);
 
 Route::post('/personnel/personnel', [PersonnelController::class, 'login']);
 
+
+
 Route::get('/login', function () {
     return view('personnel.login');
 });
+
 Route::get('/gesRecla', [ReclamationController::class, 'gesRecla']);
 
 Route::post('/update/{id}', [ReclamationController::class, 'update'])->name('update.reclamation');
 
 Route::delete('/del/{id}', [PersonnelController::class, 'del']);
 
-<<<<<<< HEAD
+
 Route::post('/personnel/logout', [PersonnelController::class, 'logout']);
-=======
 
 
 
-});
->>>>>>> 34ce1d8733faee5943f3798d5ceef512df6d15b0
+
+
+
